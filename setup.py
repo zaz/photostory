87020789877d0b8db84b1057d6375d6aef8e7009
Josh Brown <joshbrown15@gmail.com>
2010-07-21 16:58:51 +0100

    Created a setup.py and install.diff.

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# Installation script for Photostory
+
+from distutils.core import setup
+from glob import glob
+import os
+
+# Change the paths to point to the correct directories:
+os.system('patch -b app.py install.diff')
+
+setup(name='Photostory',
+      version='0.9',
+      description='Photostory lets you use your webcam to photo yourself\
+ every day, then compile them into a video.',
+      #long_description='''XXX''',
+      author='Photostory Team',
+      #author_email='https://launchpad.net/~photostory-team/+contactuser',
+      maintainer='Joel Auterson',
+      maintainer_email='joel.auterson@googlemail.com',
+      keywords=['photo', 'story', 'daily', 'picture', 'webcam', 'story',
+                'video'],
+      url='http://launchpad.net/photostory',
+      license='GNU GPL v3',
+      data_files=[ # FIXME: Create empty 'pictures' directory.
+                  ('share/photostory', ['app.py']),
+                  ('share/photostory/data', glob('data/*')),
+                  ('share/applications/', ['photostory.desktop']),
+                  ('share/icons/hicolor/scalable/apps', ['data/icon.svg']),
+                 ],
+      requires=['gst', 'gtk2']
+     )
