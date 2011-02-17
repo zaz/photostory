815d878b74d43bcc074681f70b63853c7caf3072
Josh Brown <joshbrown15@gmail.com>
2011-02-17 18:10:00 +0000

    Add notice about data format change

--- setup.py
+++ setup.py
@@ -7,7 +7,11 @@ setup(name='Photostory',
       version='1.0',
       description='Photostory lets you use your webcam to photo yourself\
  every day, then compile them into a video.',
-      #long_description='''XXX''',
+      long_description='NOTICE: The data storage format has changed between\
+ versions 0.9 and 1.0, to continue using Photostory with your previous data\
+ you will need to manually run a conversion program. The conversion program\
+ can be downloaded from <http://launchpad.net/photostory/trunk/0.9/+download/\
+photostory_0.9_to_1.0.py>.',
       author='Photostory Team',
       author_email='photostory-team@lists.launchpad.net',
       maintainer='Joel Auterson',
